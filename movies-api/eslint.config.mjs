65c28a188a3b8dc88176d051d26337a537b0f8c1
wolfgangromanowski002 <20101931@wit.ie>
2024-12-22 15:17:54 -0800

    Added Movies Route

--- movies-api/eslint.config.mjs
+++ movies-api/eslint.config.mjs
@@ -7,7 +7,7 @@ export default [
     files: ["**/*.js"],
     languageOptions: {
       globals: globals.node,
-      ecmaVersion: 2015, // ES6
+      ecmaVersion: "latest", // ES6
       sourceType: "module", // Use module format
     },
     rules: {
--- movies-api/index.js
+++ movies-api/index.js
@@ -4,15 +4,19 @@ import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
+import moviesRouter from './api/movies';   //import movies router
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT; 
 
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
+
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
