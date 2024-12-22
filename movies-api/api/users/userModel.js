7ae50004869df6857597d00e450cbb76d15f1e4e
wolfgangromanowski002 <20101931@wit.ie>
2024-12-22 15:26:10 -0800

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -18,5 +19,35 @@ const UserSchema = new Schema({
         }
     }
 });
+
+
+
+UserSchema.methods.comparePassword = async function (passw) { 
+    return await bcrypt.compare(passw, this.password); 
+  }
+
+  
+  UserSchema.statics.findByUserName = function (username) {
+    return this.findOne({ username: username });
+  };
+  
+
+  UserSchema.pre('save', async function(next) {
+    const saltRounds = 10; // You can adjust the number of salt rounds
+    //const user = this;
+    if (this.isModified('password') || this.isNew) {
+      try {
+        const hash = await bcrypt.hash(this.password, saltRounds);
+        this.password = hash;
+        next();
+    } catch (error) {
+       next(error);
+    }
+  
+    } else {
+        next();
+    }
+  });
+  
 
 export default mongoose.model('User', UserSchema);
